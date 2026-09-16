f311ada512aa89e492fdf43851e47ebb091601f2
Chandramouli <cmreddyg85@gmail.com>
2026-09-16 21:49:37 +0530

    issues fix

--- data/email.js
+++ data/email.js
@@ -1,49 +1,199 @@
 const emailData = [
   {
     subject:
-      "Bank Account Statement – Pothabathula S Ram Kumar – XXXX5649 – 01/01/2022 to 31/12/2022",
+      "Bank Account Statement – Pothabathula S Ram Kumar – XXXX5649 – 01/01/2026 to 31/07/2026",
     rowDate: null,
     senderName: "IOB eServices",
     senderEmail: "eservices@iob.in",
     headerDateTime: null,
-    logo: "https://lh3.googleusercontent.com/a-/ALV-UjW8d_wC5A5sLI8PQp9NeoyI_JocPZyhEW028Ee0qmfjfuSVF7UR=s80-p",
+    logo: "https://tse1.mm.bing.net/th/id/OIP.8kn4M9sp7Hb4T-KSgHNacQAAAA?r=0&pid=ImgDet&w=184&h=184&c=7&dpr=1.3&o=7&rm=3",
     //popup details
     date: null,
     mailedBy: "iob.in",
     signedBy: "iob.in",
     textReplacements: [
-      { find: "y.nikhileswarreddy@gmail.com", replace: "eservices@iob.in" },
-      { find: "Nikki Nikki", replace: "IOB eServices" },
-      {
-        find: "Sat, Jun 27, 2026 at 12:15 AM",
-        replace: "Sat, Jun 22, 12:15 AM",
-      },
-      {
-        find: "Sat, Jun 27, 12:15 AM (1 day ago)",
-        replace: "Sat, Jun 22, 12:15 AM",
-      },
-      {
-        find: "Jun 27, 2026, 12:15 AM",
-        replace: "Sat, Jun 22, 12:15 AM",
-      },
+      { find: "sairam9676777@gmail.com", replace: "eservices@iob.in" },
+      { find: "Sai Ram <", replace: "SaiRam <" },
+      { find: "Sai Ram", replace: "IOB eServices" },
+      // {
+      //   find: "Sat, Jun 27, 2026 at 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Sat, Jun 27, 12:15 AM (1 day ago)",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Jun 27, 2026, 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+    ],
+  },
+  {
+    subject:
+      "Bank Account Statement – Pothabathula S Ram Kumar – XXXX5649 – 01/01/2025 to 31/12/2025",
+    rowDate: null,
+    senderName: "IOB eServices",
+    senderEmail: "eservices@iob.in",
+    headerDateTime: null,
+    logo: "https://tse1.mm.bing.net/th/id/OIP.8kn4M9sp7Hb4T-KSgHNacQAAAA?r=0&pid=ImgDet&w=184&h=184&c=7&dpr=1.3&o=7&rm=3",
+    //popup details
+    date: null,
+    mailedBy: "iob.in",
+    signedBy: "iob.in",
+    textReplacements: [
+      { find: "sairam9676777@gmail.com", replace: "eservices@iob.in" },
+      { find: "Sai Ram <", replace: "SaiRam <" },
+      { find: "Sai Ram", replace: "IOB eServices" },
+      // {
+      //   find: "Sat, Jun 27, 2026 at 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Sat, Jun 27, 12:15 AM (1 day ago)",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Jun 27, 2026, 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+    ],
+  },
+  {
+    subject:
+      "Bank Account Statement – Pothabathula S Ram Kumar – XXXX5649 – 01/01/2024 to 31/12/2024",
+    rowDate: null,
+    senderName: "IOB eServices",
+    senderEmail: "eservices@iob.in",
+    headerDateTime: null,
+    logo: "https://tse1.mm.bing.net/th/id/OIP.8kn4M9sp7Hb4T-KSgHNacQAAAA?r=0&pid=ImgDet&w=184&h=184&c=7&dpr=1.3&o=7&rm=3",
+    //popup details
+    date: null,
+    mailedBy: "iob.in",
+    signedBy: "iob.in",
+    textReplacements: [
+      { find: "sairam9676777@gmail.com", replace: "eservices@iob.in" },
+      { find: "Sai Ram <", replace: "SaiRam <" },
+      { find: "Sai Ram", replace: "IOB eServices" },
+      // {
+      //   find: "Sat, Jun 27, 2026 at 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Sat, Jun 27, 12:15 AM (1 day ago)",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Jun 27, 2026, 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+    ],
+  },
+  {
+    subject:
+      "Bank Account Statement – Pothabathula S Ram Kumar – XXXX5649 – 01/01/2023 to 31/12/2023",
+    rowDate: null,
+    senderName: "IOB eServices",
+    senderEmail: "eservices@iob.in",
+    headerDateTime: null,
+    logo: "https://tse1.mm.bing.net/th/id/OIP.8kn4M9sp7Hb4T-KSgHNacQAAAA?r=0&pid=ImgDet&w=184&h=184&c=7&dpr=1.3&o=7&rm=3",
+    //popup details
+    date: null,
+    mailedBy: "iob.in",
+    signedBy: "iob.in",
+    textReplacements: [
+      { find: "sairam9676777@gmail.com", replace: "eservices@iob.in" },
+      { find: "Sai Ram <", replace: "SaiRam <" },
+      { find: "Sai Ram", replace: "IOB eServices" },
+      // {
+      //   find: "Sat, Jun 27, 2026 at 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Sat, Jun 27, 12:15 AM (1 day ago)",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Jun 27, 2026, 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
     ],
   },
   {
     subject:
       "Bank Account Statement – Pothabathula S Ram Kumar – XXXX5649 – 01/01/2022 to 31/12/2022",
-    rowDate: "May 15",
+    rowDate: null,
+    senderName: "IOB eServices",
+    senderEmail: "eservices@iob.in",
+    headerDateTime: null,
+    logo: "https://tse1.mm.bing.net/th/id/OIP.8kn4M9sp7Hb4T-KSgHNacQAAAA?r=0&pid=ImgDet&w=184&h=184&c=7&dpr=1.3&o=7&rm=3",
+    //popup details
+    date: null,
+    mailedBy: "iob.in",
+    signedBy: "iob.in",
+    textReplacements: [
+      { find: "sairam9676777@gmail.com", replace: "eservices@iob.in" },
+      { find: "Sai Ram <", replace: "SaiRam <" },
+      { find: "Sai Ram", replace: "IOB eServices" },
+      // {
+      //   find: "Sat, Jun 27, 2026 at 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Sat, Jun 27, 12:15 AM (1 day ago)",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+      // {
+      //   find: "Jun 27, 2026, 12:15 AM",
+      //   replace: "Sat, Jun 22, 12:15 AM",
+      // },
+    ],
+  },
+  {
+    subject: "Relieving Letter & Service Letter Confirmation !!!",
+    rowDate: "Jul 6",
+    headerDateTime: "Mon, Jul 6, 5:11 PM",
+    date: "Jul 6, 2026, 5:11 PM",
     textReplacements: [
       {
-        find: "Sat, Jun 27, 4:50 AM (1 day ago)",
-        replace: "Fri, May 15, 10:30 AM",
+        find: "Tue, Aug 11, 2:27 PM (9 days ago)",
+        replace: "Mon, Jul 6, 5:11 PM",
       },
       {
-        find: "Jun 27, 2026, 4:50 AM",
-        replace: "Fri, May 15, 10:30 AM",
+        find: "Tue, Aug 11, 2:27 PM (10 days ago)",
+        replace: "Mon, Jul 6, 5:11 PM",
       },
       {
-        find: "Sat, Jun 27, 2026 at 4:50 AM",
-        replace: "Fri, May 15, 10:30 AM",
+        find: "Tue, Aug 11, 2026 at 2:27 PM",
+        replace: "Mon, Jul 6, 2026 at 5:11 PM",
+      },
+      {
+        find: "Aug 11, 2026, 2:27 PM",
+        replace: "Jul 6, 2026, 5:11 PM",
+      },
+    ],
+  },
+  {
+    subject: "Employment  Offer Letter!!!",
+    rowDate: "8/4/22",
+    headerDateTime: "Mon, 8 Apr 2022, 10:54 AM",
+    date: "Apr 8, 2022, 10:54 AM",
+    textReplacements: [
+      {
+        find: "Mon, Aug 3, 5:07 PM (20 hours ago)",
+        replace: "Mon, 8 Apr 2022, 10:54 AM",
+      },
+      {
+        find: "Mon, Aug 3, 5:07 PM (21 hours ago)",
+        replace: "Mon, 8 Apr 2022, 10:54 AM",
+      },
+      {
+        find: "Aug 11, 2026, 2:29 PM",
+        replace: "8 Apr 2022, 10:54",
+      },
+      {
+        find: "Tue, Aug 11, 2026 at 2:29 PM",
+        replace: "Mon, Apr 8, 2022 at 10:54 AM",
       },
     ],
   },
